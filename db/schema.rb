b66923ad7204389ae6765cf014feaeaf7aec584d
github username <dylankershaw@me.com>
2017-12-13 15:49:32 -0500

    removed string from User schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213204615) do
+ActiveRecord::Schema.define(version: 20171213204857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "string"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
