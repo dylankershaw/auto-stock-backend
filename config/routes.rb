d5bb5e0aa68fd48122792b0b0c25aaa70e289a07
github username <dylankershaw@me.com>
2017-12-13 23:10:24 -0500

    created show routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :users
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: :show
+      resources :images, only: :show
+      resources :labels, only: :show
+      resources :imageLabels, only: :show
     end
   end
 end
