555dbefd55936c7520ba08447834bfe002b6494f
github username <dylankershaw@me.com>
2017-12-14 13:14:29 -0500

    added create route to all resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :users
   namespace :api do
     namespace :v1 do
-      resources :users, only: :show
-      resources :images, only: :show
-      resources :labels, only: :show
-      resources :image_labels, only: :show
+      resources :users, only: [:show, :create]
+      resources :image_labels, only: [:show, :create]
+      resources :images, only: [:show, :create]
+      resources :labels, only: [:show, :create]
     end
   end
 end
