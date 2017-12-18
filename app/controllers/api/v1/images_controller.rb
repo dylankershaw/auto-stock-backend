dbc317262a295023f0765b78f3b04fcf43988aa4
github username <dylankershaw@me.com>
2017-12-18 11:58:29 -0500

    images now have public url

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,7 +1,5 @@
 
-### ARE THESE REQUIRES NECESSARY?
 require "google/cloud/storage"
-require "mini_magick"
 require 'base64'
 
 class Api::V1::ImagesController < ApplicationController
@@ -11,35 +9,46 @@ class Api::V1::ImagesController < ApplicationController
     end
 
     def create
+
+        # extracts file information
         uploaded_io = params["image_io"]["base64"]
         metadata = uploaded_io.split(',/')[0] + ","
         filetype = metadata.split("/")[1].split("base64")[0][0...-1]
         base64_string = uploaded_io[metadata.size..-1]
+        filename = "#{Time.new.to_i}.#{filetype}"
+        
+        # converts from base64 string to image file
         blob = Base64.decode64(base64_string)
         image = MiniMagick::Image.read(blob)
-        filename = "#{Time.new.to_i}.#{filetype}"
-        image.write filename
         
+        # initializes google cloud storage session
         storage = Google::Cloud::Storage.new(
             project_id: ENV['GOOGLE_CLOUD_PROJECT'],
             credentials: JSON.parse(File.read('config/google_cloud_credentials.json'))
             #### NEED TO MAKE THIS REFERENCE AN ENV VAR; WON'T WORK ON HEROKU
         )
         
-        bucket = storage.bucket "auto-stock-189103.appspot.com"
-        bucket.create_file image.tempfile.path, "test/#{filename}"
+        # initializes google cloud storage bucket and uploads image
+        bucket_name = "auto-stock-189103.appspot.com"
+        bucket = storage.bucket bucket_name
+        bucket.create_file image.tempfile.path, filename
 
-        # image = Image.new
-        # image.url = (upload to google cloud)
+        # assigns public permission to file
+        file = bucket.file filename
+        file.acl.public!
+
+        new_image = Image.new
+        new_image.url = "https://storage.googleapis.com/#{bucket_name}/#{filename}"
+        byebug
         # labels = (send to google vision for labels)
         # labels.each do |label|
             # new_label = Label.find_or_create
-            # image.labels << new_label
+            # new_image.labels << new_label
             # end
-        # image.save
+        # new_image.save
         # user = User.find(params["userId"])
-        # user.images << image
+        # user.images << new_image
         # user.save
-        # render json: image
+        # render json: new_image
     end
 end
