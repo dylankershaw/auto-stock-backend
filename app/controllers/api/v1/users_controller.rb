51f7926d1c92ca180188ee4d6a07d5b112352110
github username <dylankershaw@me.com>
2017-12-19 13:12:07 -0500

    working signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user
     end
+
+    def create
+        user = User.create(username: params["username"], password: params["password"])
+        render json: {
+            id: user.id,
+            username: user.username,
+            token: JWT.encode({user_id: user.id, user_username: user.username}, ENV['JWT_SECRET'], ENV['JWT_ALGORITHM'])
+        }
+    end
 end
