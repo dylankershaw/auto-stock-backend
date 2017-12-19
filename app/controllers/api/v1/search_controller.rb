902902e1f97b9325a4d68881394e5e3a5f3db56b
github username <dylankershaw@me.com>
2017-12-19 10:54:45 -0500

    search#show controller returns score for each image

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::SearchController < ApplicationController
     def show
-        label = Label.find_by(name: params["term"].downcase)
-        render json: label.images
+        # returns all images with given label
+        images = Label.find_by(name: params["term"].downcase).images
+        
+        # writes relevancyScore for each image
+        images.map do |image|
+            image.score = image.imageLabels.find_by(image_id: image.id).relevancyScore
+        end
+
+        render json: images, methods: [:score]
     end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ApplicationRecord
     has_many :imageLabels
     has_many :labels, through: :imageLabels
+    attr_accessor :score # used to assign relevancy score during a label search
 end
